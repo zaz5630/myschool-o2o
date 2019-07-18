50cea1c5b37cf5a9a6a420a9793a00b30dd9f7b8
zaz5630 <670049410@qq.com>
2019-07-18 16:57:50 +0800

    add ajax

--- o2o-superadmin/src/main/java/com/iweb/o2o/superadmin/controller/AreaController.java
+++ o2o-superadmin/src/main/java/com/iweb/o2o/superadmin/controller/AreaController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -31,5 +32,11 @@ public class AreaController {
         //视图
         mv.setViewName("test.html");
         return mv;
+    }
+
+    @GetMapping("/testAjax")
+    @ResponseBody
+    public Object testAjax() {
+        return areaService.getAllAreas();
     }
 }
